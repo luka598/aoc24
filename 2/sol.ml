1fa0e48d0b67f8f0000f3d62f9b8797f1da5b367
luka598 <luka598@tuta.io>
2024-12-02 23:20:34 +0100

    day 2 tiny improvements

--- 2/sol.ml
+++ 2/sol.ml
@@ -1,5 +1,10 @@
+let time_it f =
+  let start = Sys.time () in
+  let res = f () in
+  (res, (Sys.time ()) -. start)
+
 let load_file path =
-  let rec load_lines channel acc = 
+  let rec load_lines channel acc =
     match In_channel.input_line channel with
     | Some value -> load_lines channel (value :: acc);
     | None -> In_channel.close channel; List.rev acc;
@@ -10,29 +15,43 @@ let load_file path =
 
 let parse_lines lines =
 (* (List.filter (fun x -> x <> "") (String.split_on_char ' ' value) *)
+  let parse_line line = 
+    line
+    |> String.split_on_char ' '
+    |> List.filter_map (fun x -> int_of_string_opt x)
+  in
+
   let rec create_report lines acc = 
     match lines with
     | [] -> List.rev acc
-    | h :: t ->
-      create_report 
-        t
-        ((List.map (fun x -> int_of_string x) (List.filter (fun x -> x <> "") (String.split_on_char ' ' h))) :: acc)
+    | h :: t -> create_report t (parse_line h :: acc)
     in
   
   create_report lines []
 
-let test_report report =
-  let increasing = (List.nth report 0) < (List.nth report 1) in
-  let rec test values =
-    match values with
-    | [] -> true
-    | [_] -> true
-    | v1 :: v2 :: t when v1 > v2 && increasing -> false
-    | v1 :: v2 :: t when v1 < v2 && not increasing -> false
-    | v1 :: v2 :: t when (abs (v1 - v2) > 3) -> false
-    | v1 :: v2 :: t when (abs (v1 - v2) < 1) -> false
-    | v1 :: v2 :: t -> test (v2 :: t)
+let count_increasing lst = 
+  let rec aux lst acc =
+    match lst with
+    | [] -> acc
+    | [_] -> acc
+    | v1 :: v2 :: v when v1 > v2 -> aux (v2 :: v) (acc + 1)
+    | v1 :: v2 :: v -> aux (v2 :: v) acc
   in
+  aux lst 0
+
+let test_report report =
+    let increasing = (List.nth report 0) < (List.nth report 1) in
+    let rec test values =
+      match values with
+      | [] -> true
+      | [_] -> true
+      | v1 :: v2 :: t when v1 > v2 && increasing -> false
+      | v1 :: v2 :: t when v1 < v2 && not increasing -> false
+      | v1 :: v2 :: t when (abs (v1 - v2) > 3) -> false
+      | v1 :: v2 :: t when (abs (v1 - v2) < 1) -> false
+      | v1 :: v2 :: t -> test (v2 :: t)
+    in
+
     test report
 
 let rec remove_nth n lst =
@@ -59,11 +78,14 @@ let sol2 reports =
 let () =
   if Array.length Sys.argv = 2 then
     let filename = Sys.argv.(1) in
-    let lines = load_file filename in
-    Printf.printf "Read %d lines from %s\n%!" (List.length lines) filename;
-    let reports = parse_lines lines in
-    Printf.printf "%d\n" (sol1 reports);
-    Printf.printf "%d\n" (sol2 reports);
+    let (lines, time) = time_it (fun () -> load_file filename) in
+    Printf.printf "Read %d lines from %s in %fs\n%!" (List.length lines) filename time;
+    let (reports, time) = time_it (fun () -> parse_lines lines) in
+    Printf.printf "Parsed reports in %fs\n%!" time;
+    let (sol1', time) = time_it (fun () -> sol1 reports) in
+    Printf.printf "[1] %d in %fs\n%!" sol1' time;
+    let (sol2', time) = time_it (fun () -> sol2 reports) in
+    Printf.printf "[2] %d in %fs\n%!" sol2' time;
 
   else
     exit 1;
